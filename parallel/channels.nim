c7b732b812cdde793a1a911aa8c533254b013a0a
Riccardo Marotti <riccardo.marotti@gmail.com>
2022-01-12 14:59:37 +0100

    ADd channels examples

---
+++ parallel/channels.nim
@@ -0,0 +1,11 @@
+import os, threadpool
+var chan: Channel[string]
+open(chan)
+
+proc sayHello() =
+  sleep(1000)
+  chan.send("Hello!")
+
+spawn sayHello()
+
+doAssert chan.recv() == "Hello!"
---
+++ parallel/incrementer.nim
@@ -0,0 +1,19 @@
+import threadpool
+
+var resultChan: Channel[int]
+open(resultChan)
+
+proc increment(x: int) =
+  var counter = 0
+  for i in 0 ..< x:
+    counter.inc
+  resultChan.send(counter)
+
+spawn increment(10_000)
+spawn increment(10_000)
+spawn increment(10_000)
+sync()
+var total = 0
+for i in 0 ..< resultChan.peek:
+  total.inc resultChan.recv()
+echo(total)
